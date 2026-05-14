ed378b1831abc7164a33670b19dfbe70a601a430
imhsart <nitinrana2519@gmail.com>
2026-05-14 17:59:11 +0530

    Assignment attempt 2

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,12 @@ import './../styles/App.css';
 const App = () => {
 const dataChangRef = useRef(new Set())
   function handleChange(e){
-    dataChangRef.current.add(e.target.dataset.row)
+    dataChangRef.current.add(Number(e.target.dataset.row))
   }
   function handleSubmit(e){
     e.preventDefault()
 
-    console.log(dataChangRef.current)
+    console.log('Edited rows:', [...dataChangRef.current])
   }
   return (
     <div>
@@ -27,33 +27,33 @@ const dataChangRef = useRef(new Set())
               <tbody>
                 <tr>
                   <td>1</td>
-                  <td><input data-row='1' type="text" onChange={handleChange} /></td>
-                  <td><input data-row='1' type="number" onChange={handleChange} /></td>
+                  <td><input data-row='1' type="text" onChange={handleChange} defaultValue={'Ram'} /></td>
+                  <td><input data-row='1' type="number" onChange={handleChange} defaultValue={25} /></td>
                 </tr>
                 <tr>
                   <td>2</td>
-                  <td><input data-row='2' type="text" onChange={handleChange} /></td>
-                  <td><input data-row='2' type="number" onChange={handleChange} /></td>
+                  <td><input data-row='2' type="text" onChange={handleChange} defaultValue={'Shyam'} /></td>
+                  <td><input data-row='2' type="number" onChange={handleChange} defaultValue={30} /></td>
                 </tr>
                 <tr>
                   <td>3</td>
-                  <td><input data-row='3' type="text" onChange={handleChange} /></td>
-                  <td><input data-row='3' type="number" onChange={handleChange} /></td>
+                  <td><input data-row='3' type="text" onChange={handleChange} defaultValue={'Ali'} /></td>
+                  <td><input data-row='3' type="number" onChange={handleChange} defaultValue={35} /></td>
                 </tr>
                 <tr>
                   <td>4</td>
-                  <td><input data-row='4' type="text" onChange={handleChange} /></td>
-                  <td><input data-row='4' type="number" onChange={handleChange} /></td>
+                  <td><input data-row='4' type="text" onChange={handleChange} defaultValue={'Shaw'} /></td>
+                  <td><input data-row='4' type="number" onChange={handleChange} defaultValue={20} /></td>
                 </tr>
                 <tr>
                   <td>5</td>
-                  <td><input data-row='5' type="text" onChange={handleChange} /></td>
-                  <td><input data-row='5' type="number" onChange={handleChange} /></td>
+                  <td><input data-row='5' type="text" onChange={handleChange} defaultValue={'Tavneet'} /></td>
+                  <td><input data-row='5' type="number" onChange={handleChange} defaultValue={50} /></td>
                 </tr>
                 <tr>
                   <td>6</td>
-                  <td><input data-row='6' type="text" onChange={handleChange} /></td>
-                  <td><input data-row='6' type="number" onChange={handleChange} /></td>
+                  <td><input data-row='6' type="text" onChange={handleChange} defaultValue={'Lakshmi'} /></td>
+                  <td><input data-row='6' type="number" onChange={handleChange} defaultValue={40} /></td>
                 </tr>
               </tbody>
             </table>
